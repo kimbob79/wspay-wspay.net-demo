b79843a8a9a46fdd2341a7e47f54cd0a7aba9bab
WJW <kimbob79@gmail.com>
2026-01-19 13:54:14 +0900

    11

--- member.php
+++ member.php
@@ -107,7 +107,7 @@
 	$xlsx_sql = "select * {$sql_common} {$sql_search} {$sql_order} ";
 	$result = sql_query($sql);
 
-	echo $sql;
+	// echo $sql;
 //	echo $total_pay;
 
 if($member['mb_level']==10){
